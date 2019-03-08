731624d85f10a04fa95a988a70bbf54876d8c808
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-08 17:07:51 -0500

    Print 10*4=40

--- multiply.asm
+++ multiply.asm
@@ -1,7 +1,12 @@
 .data
 	
 .text 
-	mul
-	mult
-	sll
+	addi	$s0, $zero, 10	#s0 = 0 + 10
+	addi	$s1, $zero, 4	#s1 = 4
 	
+	mul	$t0, $s0, $s1	#t0 = s0 * s1
+	
+	#print result
+	li	$v0, 1
+	add	$a0, $zero, $t0
+	syscall
