8361de03decc17f43ca3904fe6fb2d270e86245b
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-08 16:55:22 -0500

    Create myAdder.asm

---
+++ myAdder.asm
@@ -0,0 +1,10 @@
+.data
+	numberOne:	.word 5		#declare integer words
+	numberTwo:	.word 10
+.text
+	lw	$t0, numberOne($zero)
+	lw	$t1, numberTwo($zero)
+	
+	add	$t2, $t0, $t1		#store the sum of t0,t1 into t2
+	
+	
