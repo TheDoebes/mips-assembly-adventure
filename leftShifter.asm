11a10eb8ef4e6767c92d5864aca928a045707af9
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-14 20:06:47 -0400

    Complete leftShifter.asm

--- leftShifter.asm
+++ leftShifter.asm
@@ -1,4 +1,11 @@
 .data
 
 .text
-	sll	
+	addi	$s0, $zero, 4	#s0 = 2;
+	
+	sll	$t0, $s0, 2	#t0 = s0 * 2^n, n = 2: t0 = 4 * 4 = 16;
+	
+	#Print results
+	li	$v0, 1		#set print type to integer
+	add	$a0, $zero, $t0	#set t0 to the print buffer
+	syscall			#Do It! 
