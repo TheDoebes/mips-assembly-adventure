963a04ef514826b978a4995a168ab5cf4251ffd5
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-14 20:14:41 -0400

    Update myDivision.asm

--- myDivision.asm
+++ myDivision.asm
@@ -4,4 +4,9 @@
 	addi	$t0, $zero, 30	#store 30 in t0
 	addi	$t1, $zero, 5	#store 5 in t1
 	
+	div	$s0, $t0, $t1	#s0 = t0 / t1 = 30 / 5 = 6
 	
+	#Display Results
+	li	$v0, 1			#type int
+	add	$a0, $zero, $s0		#buffer s0
+	syscall				#Print
