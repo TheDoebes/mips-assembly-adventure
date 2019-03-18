d50662f6ab24f56b0a29514583ecddcd7682d2c4
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-18 12:42:49 -0400

    Create exit code and first function

--- procedure.asm
+++ procedure.asm
@@ -2,3 +2,15 @@
 	message:	.asciiz "Hi everybody! \nMy name is Cade.\n"
 .text
 	main:	
+	
+	
+	#Exit Code - required to avoide an infinite loop with displayMessage
+	li	$v0, 10
+	syscall
+	
+	displayMessage:	
+		li	$v0, 4
+		la	$a0, message
+		syscall
+		
+		jr	$ra
