30d4b336308f8fa3c8d6e9d4616aff2f75947036
TheDoebes <TheDoebes@users.noreply.github.com>
2019-03-23 13:12:05 -0400

    Create recursion.asm

---
+++ Assembly-Badge/recursion.asm
@@ -0,0 +1,29 @@
+.data
+
+.text
+
+	fact:	
+		addi	$sp, $sp, -8	#Create two spots on the stack
+		sw	$ra, 4($sp)	#Preserve the return address on the stack (this is necassary for recursion)
+		sw	$a0, 0($s0)	#Preserve the argument of the current iteration
+		
+		slt	$t0, $a0, 1		#Create flag at t0 = (a0 < 1)
+		beq	$t0, $zero, IfThen	#If t0 flag is true, then go to label IfThen
+		
+		#Else,
+		addi	$v0m $zero, 1	#Load 1 into return register
+		addi	$sp, $sp, 8	#Pop the top two items off the stack to return it to its original state
+		jr	$ra		#return to address which called the routine
+		
+		IfThen:	
+			#Call fact(n-1)
+			addi	$a0, $a0, -1	
+			jal	fact		
+			
+			#restore the prior state data before leaving the function
+			lw	$a0, 0($sp)	#return the argument register
+			lw	$a0, 4($sp)	#return the retunr address
+			addi	$sp, $sp, 8	#pop the two items off the stack since they aren't needed anymore
+			
+			mul	$v0, $a0, $v0	#compute the multiplication part of v0 = fact(n) * fact(n-1)
+			jr	$ra		#return to the address which called the routine
